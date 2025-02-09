c41f01b33dfeded68629546479a9bf8462474413
TsitouhRanjafy <radohery09@gmail.com>
2025-02-09 13:45:46 +0100

    uses cors

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import cookieParser from 'cookie-parser';
 import { OTPDataAcces, syncMongoDB, userGetDataAcces, userPostDataAcces, userPutDataAccess } from './data access';
 import { OTPRoutes, UserRouteGet, UserRoutePost, UserRoutePut } from './routes';
 import { OTPService, userGetService, userPostService, UserPutService } from './services';
+import cors from 'cors'
 
 dotenv.config();
 const app: Application = express();
@@ -15,6 +16,12 @@ const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/chatap
 const router = express.Router()
 
 // Middleware
+app.use(cors({
+    origin: process.env.DOMAIN_ORIGIN,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: [ 'Content-type','Authorization' ]  
+}));
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
